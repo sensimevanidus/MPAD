35f022a30da434351a09d73283933e14881c272b
Onur Yaman <onuryaman@gmail.com>
2011-12-11 22:31:21 +0200

    Created the Netbeans project

---
+++ src/MPAD/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: onuryaman
+ *
+ * Created on December 11, 2011, 10:30 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
